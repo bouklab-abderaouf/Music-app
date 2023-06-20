4cbcfcc79017a1faa185f6f236a1f54b209092e2
Raouf <abderaouf-hocine.bouklab@epitech.eu>
2023-06-20 16:16:07 +0200

    Security update
    revoked my api key and Used the new one as an environment variable

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -5,7 +5,7 @@ export const shazamCoreApi = createApi({
   baseQuery: fetchBaseQuery({ 
     baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', 'e640827c79mshdb7099e9f16a86bp100c3cjsne414a985a046');
+      headers.set('X-RapidAPI-Key', process.env.API_KEY);
       headers.set('X-RapidAPI-Host', 'shazam-core.p.rapidapi.com');
       return headers;
     },
